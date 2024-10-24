22949c81ca1bf05b123435e05172e471c37fe0b4
JackF710 <jtfeery@gmail.com>
2024-10-24 13:58:19 -0400

    fsbdgg

--- binary.js
+++ binary.js
@@ -1,3 +1,20 @@
 /*
-    guard clause: if d > 255, return 
-    
+    take binary number
+    count starting at 2^0 and loop until 2^7
+    */
+
+function binaryDecimal(b, binary) {
+  if (b > 8) {
+    return;
+  }
+  let exponent = 0;
+  let binary = 2;
+  for() {
+      if (b === "1") {
+      exponent++;
+    }
+  }
+  return i;
+}
+
+console.log(binaryDecimal(8, "10010110"));
